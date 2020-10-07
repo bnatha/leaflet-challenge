20162da8e76e275ba084b7c841c3fb0f57fff071
bnatha <64889421+bnatha@users.noreply.github.com>
2020-10-07 23:09:45 +1100

    Adding files - initial commit

---
+++ bonus.js
@@ -0,0 +1,92 @@
+// Define the URLs for both earthquakes and tectonic plates
+var earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var tectonicURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+var earthquakes = new L.LayerGroup()
+    var tplates = new L.LayerGroup()
+
+// Creating map object
+var myMap = L.map("map", {
+    center: [32.7157, -117.1611],
+    zoom: 7
+
+});
+
+var overlays = {
+    "Tectonic Plates" : tplates,
+    Earthquakes : earthquakes
+  };
+
+L.control.layers(overlays).addTo(myMap);
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+
+// Perform a GET request to the query URL
+d3.json(earthquakeURL, function (data) {
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: markerSize(feature.properties.mag),
+                fillColor: getColor(feature.properties.mag),
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
+        },
+
+        // Called on each feature
+        onEachFeature: function (feature, layer) {
+
+            // Giving each feature a pop-up with information pertinent to it
+            layer.bindPopup("<h1>" + feature.properties.mag + "</h1> <hr> <h2>" + feature.properties.place + "</h2>");
+
+        }
+    }).addTo(earthquakes);
+    earthquakes.addTo(myMap);
+
+    // Set up the legend
+    var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (myMap) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 1, 2, 3, 4, 5],
+            labels = [];
+        // loop through density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    legend.addTo(myMap);
+
+    d3.json(tectonicURL, function (data) {
+        L.geoJson(data, {
+
+        }).addTo(tplates);
+        tplates.addTo(myMap);
+    })
+});
+
+function markerSize(mag) {
+    return mag * 4
+}
+
+function getColor(i) {
+    return i > 5 ? "#EA2C2C" :
+        i > 4 ? "#EA822C" :
+            i > 3 ? "#EE9C00" :
+                i > 2 ? "#EECC00" :
+                    i > 1 ? "#D4EE00" :
+                        "#98EE00";
+}
---
+++ config.js
@@ -0,0 +1,1 @@
+const API_KEY = "pk.eyJ1IjoiYm5hdGhhbmllbCIsImEiOiJja2VzY3dicXkyNXIyMnRxbXFsamRtdmoyIn0.z8u6OXwsL5Yyo61gKO6dZQ"
